3a5795b9491263df62c98d52a17768080bd61d4e
Emanuele Penteado <163652019+manupenteado@users.noreply.github.com>
2025-05-04 23:26:41 +0000

    vercel

--- exemple_requests/POST_Register_User.sh
+++ exemple_requests/POST_Register_User.sh
@@ -1,4 +1,4 @@
 curl --request POST \
---url http://localhost:3000/users/register \
+--url https://express-backend-postgre-sql.vercel.app/users/register \
 --header "Content-Type: application/json" \
   --data '{"name": "manuzinha", "password": "12345678", "email":"penteadoemanuele@gmail.com"}'
